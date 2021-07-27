93220bd4a5ecaba94704ede28099e148beaa1d9c
datta-agni <dattadunga@gmail.com>
2021-07-28 00:31:24 +0530

    Tower of Hanoi!

---
+++ Code/tower_of_hanoi.py
@@ -0,0 +1,14 @@
+def hanoi(number_disks, startPeg=1, endPeg=3):
+    if number_disks:
+        hanoi(number_disks-1, startPeg, 6-startPeg-endPeg)
+        print("Move disk %d from peg %d to peg %d" %
+              (number_disks, startPeg, endPeg))
+        hanoi(number_disks-1, 6-startPeg-endPeg, endPeg)
+
+
+def main():
+    hanoi(number_disks=int(input("Enter the number of discs: ")))
+
+
+if __name__ == '__main__':
+    main()
