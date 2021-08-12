b9dd5ebd0a70a255dc5feb3808d87bf2ae2a29d2
datta-agni <dattadunga@gmail.com>
2021-08-12 19:27:24 +0530

    Kilometers to Miles conversion and vice versa.

---
+++ Codes/mile_kilometre_conversion.py
@@ -0,0 +1,26 @@
+# Taking kilometers input from the user
+def main(
+    choice=int(
+        input("Enter your choice \n1) Kilometres to Miles: \n2) Miles to Kilometres: ")
+    ),
+):
+    # conversion factor
+    conversion_fac = 0.621371
+
+    if choice == 1:
+        kilometers = float(input("Enter value in kilometers: "))
+        # calculate miles
+        miles = kilometers * conversion_fac
+        print("%0.2f kilometers is equal to %0.2f miles" % (kilometers, miles))
+    elif choice == 2:
+        miles = float(input("Enter value in kilometers: "))
+        # calculate kilometres
+        kilometers = miles // conversion_fac
+        print("%0.2f miles is equal to %0.2f miles" % (miles, kilometers))
+    else:
+        print("Wrong Choice!")
+        quit()
+
+
+if __name__ == "__main__":
+    main()
