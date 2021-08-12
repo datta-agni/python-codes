3fdb3c9df0990c3c2df98632c97eff8e0801f176
datta-agni <dattadunga@gmail.com>
2021-08-12 20:26:40 +0530

    Armstrong Number between limits.

---
+++ Codes/amstrong_number_limits.py
@@ -0,0 +1,25 @@
+# Program to check Armstrong numbers in a certain interval
+
+
+def armstrong_limits(
+    lower=int(input("Enter the lower limit: ")),
+    upper=int(input("Enter the upper limit: ")) + 1,
+):
+    for num in range(lower, upper):
+        # order of number
+        order = len(str(num))
+        # initialize sum
+        sum = 0
+
+        temp = num
+        while temp > 0:
+            digit = temp % 10
+            sum += digit ** order
+            temp //= 10
+
+        if num == sum:
+            print(num)
+
+
+if __name__ == "__main__":
+    armstrong_limits()
