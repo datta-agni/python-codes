8a3e0c2610f2284d209987e7fa61f700a4474478
datta-agni <dattadunga@gmail.com>
2021-08-12 22:47:41 +0530

    Find powers of '2'. Useful for finding 2^n.

---
+++ Codes/powers_of_two.py
@@ -0,0 +1,13 @@
+# Display the powers of 2 using anonymous function
+
+
+def powers_of_two(terms=int(input("How many terms? "))):
+    # use anonymous function
+    result = list(map(lambda x: 2 ** x, range(terms)))
+    print("The total terms are:", terms)
+    for i in range(terms):
+        print("2 raised to power", i, "is", result[i])
+
+
+if __name__ == "__main__":
+    powers_of_two()
