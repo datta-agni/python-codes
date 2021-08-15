8836b47baff9fa936b3953792b6220ce1be752bb
datta-agni <dattadunga@gmail.com>
2021-08-16 01:18:49 +0530

    Reformatted.

--- Codes/factorial_number.py
+++ Codes/factorial_number.py
@@ -1,15 +1,15 @@
-# program to find the factorial of a number provided by the user.
-# change the value for a different result num = a
+# program to find the factorial of a number provided by the user, change the value for a different result num = a
 
 
 def print_fact(num):
     factorial = 1
     # check if the number is negative, positive or zero
     if num < 0:
-        return -1
+        return None
 
     elif num == 0:
         return 0
+
     else:
         for i in range(1, num + 1):
             factorial = factorial * i
