8468956814aeb811be236d2d21a8f3050b7e5e9c
Agni Datta <dattadunga@gmail.com>
2021-08-15 18:43:41 +0530

    Cleaned up the code.

--- Codes/leap_year.py
+++ Codes/leap_year.py
@@ -2,16 +2,13 @@
 
 
 def is_leapyear(year):
-    if year % 4 == 0 and (year % 100 != 0 and year % 400 == 0):
-        return True
-    else:
-        False
+    return year % 4 == 0 and (year % 100 != 0 and year % 400 == 0)
 
 
 if __name__ == "__main__":
     # To get year (integer input) from the user
     year = int(input("Enter a year: "))
-    if is_leapyear(year) == True:
+    if is_leapyear(year):
         print(year, " is a leap year.")
     else:
         print(year, " is not a leap year.")
