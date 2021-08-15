b37f61dcec614ee554d7659e23dc12936846f242
Agni Datta <dattadunga@gmail.com>
2021-08-15 18:44:30 +0530

    Removed Logical Error.

--- Codes/leap_year.py
+++ Codes/leap_year.py
@@ -2,7 +2,7 @@
 
 
 def is_leapyear(year):
-    return year % 4 == 0 and (year % 100 != 0 and year % 400 == 0)
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
 
 if __name__ == "__main__":
